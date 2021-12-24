0c5a3c7b5e02743120e045f54a3c8e550f593d84
chumochaOUO <chumochaOUO@gmail.com>
2021-12-24 10:41:42 +0800

    對話完成!
    BLA

--- hi2 unity/Assets/Scripts/DialogueSystem.cs
+++ hi2 unity/Assets/Scripts/DialogueSystem.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 ///<summary>
@@ -11,14 +12,26 @@ public class DialogueSystem : MonoBehaviour
     [Header("對話間隔"), Range(0, 1)]
     public float interval = 0.3f;
     //interval=打字文字時間
+    [Header("畫布對話系統")]
+    public GameObject goDialogue;
+    [Header("對話內容")]
+    public Text textContent;
+
+    private void Start()
+    {
+        StartCoroutine(TypeEffect());
+    }
 
     private IEnumerator TypeEffect()
     {
         string test = "哈囉，你好~";
 
+        textContent.text = "";              //清除上次對話內容
+        goDialogue.SetActive(true);         //顯示對話物件
+
         for (int i = 0;i < test.Length; i++)
         {
-            print(test[i]);
+            textContent.text += (test[i]);  //疊加對話內容文字介面
             yield return new WaitForSeconds(interval);
         }
     }
